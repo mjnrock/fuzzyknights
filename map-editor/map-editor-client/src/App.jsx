d82c83612961e862c266ecef65b13e2c20bedd28
LesPantsFancy <mjnrock@umich.edu>
2023-05-28 11:50:07 -0400

    init

--- map-editor/map-editor-client/src/App.jsx
+++ map-editor/map-editor-client/src/App.jsx
@@ -1,9 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+
+export function Home() {
+	return (
+		<h1 className="text-3xl font-bold underline">
+			Hello world!
+		</h1>
+	)
+}
+export function Home2() {
+	return (
+		<h1 className="text-3xl font-bold underline">
+			Hello world2!
+		</h1>
+	)
+}
+
 export function App() {
 	return (
-		<div className="text-3xl font-bold underline">
-			Hello world!
-		</div>
+		<Routes>
+			<Route index element={ <Home /> } />
+			<Route path="test" element={ <Home2 /> } />
+		</Routes>
 	);
+	;
 }
 
 export default App;
