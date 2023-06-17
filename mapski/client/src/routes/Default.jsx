dc3d6c11e11766a9725e4a9157efb1a921f18f83
LesPantsFancy <mjnrock@umich.edu>
2023-06-16 21:03:31 -0400

    init

--- mapski/client/src/routes/Default.jsx
+++ mapski/client/src/routes/Default.jsx
@@ -7,19 +7,19 @@ import Tile from "../v2/modules/map/Tile";
 import TileMapData from "../v2/modules/map/Map";
 import TileMapJSX from "../v2/components/TileMap";
 
-const dataRegistry = Registry(Identity().Create({ tags: [ [ "cat", 1 ], 5 ] })).Create({
+export const DataRegistry = Registry(Identity().Create({ tags: [ [ "cat", 1 ], 5 ] })).Create({
 	map: TileMapData().Create({
 		tileData: (x, y) => Math.floor(Math.random() * 5),
 	}),
 });
 
-console.log(dataRegistry);
-console.log(QueryRegistry(dataRegistry).getByAlias("map"));
+console.log(DataRegistry);
+console.log(QueryRegistry(DataRegistry).getByAlias("map"));
 
 export function Default() {
 	return (
 		<>
-			<TileMapJSX data={ QueryRegistry(dataRegistry).getByAlias("map") } />
+			<TileMapJSX data={ QueryRegistry(DataRegistry).getByAlias("map") } />
 		</>
 	);
 };
