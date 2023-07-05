8c94202942c99bc7fab203064eba42f41009d390
LesPantsFancy <mjnrock@umich.edu>
2023-07-05 08:33:51 -0400

    init

--- update-mapski-client.ps1
+++ update-mapski-client.ps1
@@ -7,5 +7,23 @@ $directoryPath = Join-Path -Path $scriptPath -ChildPath ".\mapski\client"
 # Change to the directory
 Set-Location -Path $directoryPath
 
-# Run yarn start
+# Update the git repository
+Write-Host "Pulling latest changes from git `master` branch..."
+git pull origin master
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Failed to pull latest changes from git `master` branch"
+    exit $LASTEXITCODE
+}
+
+# Run yarn install
+Write-Host "Running `yarn install`..."
 Invoke-Expression "yarn install"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Failed to run `yarn install` command"
+    exit $LASTEXITCODE
+}
+
+Write-Host "Script completed successfully."
+
+# Wait for the user to press Enter before exiting
+Read-Host "Press Enter to exit"
