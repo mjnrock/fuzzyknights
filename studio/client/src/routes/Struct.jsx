b16ce749c21606986a8d7172096b64b6901d5eaf
LesPantsFancy <mjnrock@umich.edu>
2023-07-11 21:32:39 -0400

    init

--- studio/client/src/routes/Struct.jsx
+++ studio/client/src/routes/Struct.jsx
@@ -1,28 +1,30 @@
+import { useState } from "react";
 import { Table } from "../apps/structski/modules/crud/Table";
 import Record from "../apps/structski/modules/crud/Record";
 
 export function Struct() {
+	const [ data, setData ] = useState({
+		"foo": "bar",
+		"baz": {
+			"qux": "quux",
+			"corge": "grault",
+			"array": [
+				"one",
+				"two",
+			],
+			"garply": {
+				"waldo": "fred",
+				"plugh": "xyzzy",
+				"thud": "wibble"
+			}
+		}
+	});
+
 	return (
 		<>
 			<div className="flex flex-col items-center justify-center m-2 overflow-hidden border border-solid rounded shadow-md select-none border-neutral-200">
 				<Record
-					data={ {
-						"foo": "bar",
-						"baz": {
-							"qux": "quux",
-							"corge": "grault",
-							"array": [
-								"one",
-								"two",
-								"three"
-							],
-							"garply": {
-								"waldo": "fred",
-								"plugh": "xyzzy",
-								"thud": "wibble"
-							}
-						}
-					} }
+					data={ data }
 				/>
 			</div>
 
