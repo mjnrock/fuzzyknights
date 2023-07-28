ede69454ee692091d09c86b0108414f51d28cf2c
LesPantsFancy <mjnrock@umich.edu>
2023-07-27 21:26:06 -0400

    init

---
+++ game/dotf/client/craco.config.js
@@ -0,0 +1,9 @@
+// const path = require("path");
+
+// module.exports = {
+// 	webpack: {
+// 		alias: {
+// 			_outsideSrc: path.resolve(__dirname, "../path-to-your-directory-outside-src"),
+// 		}
+// 	}
+// };
