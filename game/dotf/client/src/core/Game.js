e51e768149923ec7ee1c4b94402b6e883843c88b
LesPantsFancy <mjnrock@umich.edu>
2023-07-31 20:34:14 -0400

    init

--- game/dotf/client/src/core/Game.js
+++ game/dotf/client/src/core/Game.js
@@ -1,3 +1,4 @@
+import * as PIXI from "pixi.js";
 import Node from "../@node/Node";
 
 import { GameLoop } from "./GameLoop.js";
@@ -5,16 +6,29 @@ import { GameLoop } from "./GameLoop.js";
 export class Game extends Node {
 	static Instances = new Map();
 	static GetInstance(id) {
+		let game;
 		if(id) {
 			return Game.Instances.get(id);
+		} else {
+			game = Game.Instances.values().next().value;
 		}
 
-		return Game.Instances.values().next().value;
+		return game;
 	}
 
-	constructor ({ loop = {}, ...self } = {}) {
+	constructor ({ pixi = {}, loop = {}, ...self } = {}) {
 		super({ ...self });
 
+		//TODO: Ready to go, just need to refactor to use it
+		// this.pixi = new PIXI.Application({
+		// 	hello: "world",
+
+		// 	backgroundColor: 0x000000,
+		// 	resizeTo: window,
+		// 	antialias: true,
+		// 	preserveDrawingBuffer: true,
+		// 	...pixi,
+		// });
 		this.loop = new GameLoop({
 			...loop,
 
