99af257a1bf9ce9c3961e3c3bc33794857ebc150
LesPantsFancy <mjnrock@umich.edu>
2023-08-23 20:00:01 -0400

    poc

--- studio/client/src/App.jsx
+++ studio/client/src/App.jsx
@@ -7,15 +7,13 @@ import { Struct as StructRoute } from "./routes/Struct.jsx";
 import { Spriteski as SpriteskiRoute } from "./routes/Spriteski.jsx";
 import { Sandbox as SandboxRoute } from "./routes/Sandbox.jsx";
 
-// FIXME: Once you integrate the Node changes from Game into Chord, you *must* set the `config` allowShallowPrevious to `false` AND allowTrivialUpdate to `false`; otherwise state history will be mutated
-// FIXME: Once you integrate the Node changes from Game into Chord, you *must* set the `config` allowShallowPrevious to `false` AND allowTrivialUpdate to `false`; otherwise state history will be mutated
-// FIXME: Once you integrate the Node changes from Game into Chord, you *must* set the `config` allowShallowPrevious to `false` AND allowTrivialUpdate to `false`; otherwise state history will be mutated
-// FIXME: Once you integrate the Node changes from Game into Chord, you *must* set the `config` allowShallowPrevious to `false` AND allowTrivialUpdate to `false`; otherwise state history will be mutated
-// FIXME: Once you integrate the Node changes from Game into Chord, you *must* set the `config` allowShallowPrevious to `false` AND allowTrivialUpdate to `false`; otherwise state history will be mutated
-// FIXME: Once you integrate the Node changes from Game into Chord, you *must* set the `config` allowShallowPrevious to `false` AND allowTrivialUpdate to `false`; otherwise state history will be mutated
-// NOTE: Editor won't work correctly until you do this.
+import SqlHelper from "./lib/SqlHelper.js";
 
-// NOTE: (From the future): Testing seems to suggest that the above is *not* required -- do more rigorous testing to confirm this (but the refactor may have fixed this issue)
+setTimeout(() => {
+	SqlHelper.query(`SELECT GETDATE() AS CurrentDateTime`).then(result => console.log(result));
+	SqlHelper.exec(`spTEST`).then(result => console.log(result));
+	SqlHelper.tvf(`tvfTEST`).then(result => console.log(result));
+}, 500);
 
 export function App() {
 	return (
---
+++ studio/server/routes/sql.js
@@ -0,0 +1,91 @@
+import express from "express";
+import path from "path";
+import SqlExecutor, { localhostConfig } from "../lib/SqlExecutor.js";
+
+export function main(wd) {
+	wd = path.join(wd, "sql");
+
+	const sql = new SqlExecutor({
+		...localhostConfig,
+		server: process.env.MSSQL_SERVER,
+		database: process.env.MSSQL_DATABASE,
+		user: process.env.MSSQL_USERNAME,
+		password: process.env.MSSQL_PASSWORD,
+	});
+
+	const routes = [
+		{
+			methods: [ "POST" ],
+			path: "/exec",
+			handlers: [
+				async (req, res) => {
+					try {
+						const {
+							sproc,
+							params = {},
+						} = req.body;
+
+						const result = await sql.exec(sproc, params);
+
+						res.json(result);
+					} catch(err) {
+						res.status(500).json({ error: err.message });
+					}
+				},
+			],
+		},
+		{
+			methods: [ "POST" ],
+			path: "/query",
+			handlers: [
+				async (req, res) => {
+					try {
+						const {
+							query,
+							params = [],
+						} = req.body;
+
+						const result = await sql.query(query, params);
+
+						res.json(result);
+					} catch(err) {
+						res.status(500).json({ error: err.message });
+					}
+				},
+			],
+		},
+		{
+			methods: [ "POST" ],
+			path: "/tvf",
+			handlers: [
+				async (req, res) => {
+					try {
+						const {
+							name,
+							params = [],
+						} = req.body;
+
+						const result = await sql.tvf(name, params);
+
+						console.log(result)
+
+						res.json(result);
+					} catch(err) {
+						res.status(500).json({ error: err.message });
+					}
+				},
+			],
+		},
+	];
+
+	const router = express.Router();
+	for(const route of routes) {
+		for(const method of route.methods) {
+			router[ method.toLowerCase() ](route.path, ...route.handlers);
+		}
+	}
+
+	return router;
+};
+
+export default main;
